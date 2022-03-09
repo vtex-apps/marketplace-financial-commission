0fb71b5ebc28cb0d3dc2ee6bdaa010e63e1920a1
deimer.marin <deimer.marin@vtex.com.br>
2022-03-09 10:09:04 -0500

    Resolved conflicts

--- node/clients/sellers/index.ts
+++ node/clients/sellers/index.ts
@@ -33,7 +33,11 @@ export default class SellersIO extends AppGraphQLClient {
       ...options,
       headers: {
         ...options?.headers,
+<<<<<<< HEAD
         Cookie: `VtexIdclientAutCookie=${context.adminUserAuthToken}`,
+=======
+        cookie: `VtexIdclientAutCookie=${context.authToken}`,
+>>>>>>> 87d3d8e (Commit get sellers via GrapghQL)
       },
     })
   }

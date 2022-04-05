502dac263d508adbc48f876dd7b5cf82357c7848
deimer.marin <deimer.marin@vtex.com.br>
2022-04-05 10:41:47 -0500

    Commit search orders

---
+++ node/middlewares/orders/orders.ts
@@ -0,0 +1,30 @@
+import { orderDetailCommission } from './orderDetailCommission'
+import { orderListSeller } from './orderListSeller'
+
+export async function orders(ctx: Context, next: () => Promise<Sellers>) {
+  const {
+    query: { sellerName, dateStart, dateEnd, page, perpage },
+  } = ctx
+
+  const listOrders = await orderListSeller(
+    ctx,
+    sellerName,
+    dateStart,
+    dateEnd,
+    page,
+    perpage
+  )
+
+  const ordersDetailCommission = await orderDetailCommission(ctx, listOrders)
+
+  const resultDetail = {
+    data: ordersDetailCommission,
+    paging: listOrders.paging,
+  }
+
+  ctx.status = 200
+  ctx.body = resultDetail
+  ctx.set('Cache-Control', 'no-cache ')
+
+  await next()
+}

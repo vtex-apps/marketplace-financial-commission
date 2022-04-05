149b9ed41987bcea54cac7db4de0cb5eb4e85982
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-04-05 22:23:42 +0200

    feat: new service

--- react/commissionReport.tsx
+++ react/commissionReport.tsx
@@ -1,17 +1,13 @@
 import type { FC } from 'react'
 import React, { useState } from 'react'
-import {
-  Layout,
-  PageBlock,
-  IconShoppingCart
-} from 'vtex.styleguide'
-
+import { Layout, PageBlock, IconShoppingCart } from 'vtex.styleguide'
 import { useRuntime } from 'vtex.render-runtime'
 import type { InjectedIntlProps } from 'react-intl'
 import { injectIntl } from 'react-intl'
 import { formatIOMessage } from 'vtex.native-types'
+
 import { message } from './utils/definedMessages'
-//import TableComponent from './components/Dashboard/Table'
+// import TableComponent from './components/Dashboard/Table'
 import TableComponent from './components/Dashboard/Table/Tablev2'
 import Totalizer from './components/Dashboard/Totalizer'
 import Filter from './components/Dashboard/Filter'
@@ -26,32 +22,48 @@ const CommissionReport: FC<InjectedIntlProps> = ({ intl }) => {
       <div className="mt9">
         <h1 style={{ color: '#3F3F40', fontSize: '35px' }}>
           {formatIOMessage({
-            id: message.title.id,
+            id: message.dashboardTitle.id,
             intl,
           }).toString()}
         </h1>
       </div>
       <div>
-        <SettingsDashboard openModal={openModal} setOpenModal={setOpenModal}/>
+        <SettingsDashboard openModal={openModal} setOpenModal={setOpenModal} />
       </div>
       <div className="mt4">
         <PageBlock>
           <div className="mt4 mb7">
-            <Filter listSellers={[]} sellersDashboard={[]} startDatePicker={new Date()} finalDate={new Date()} locale={culture.locale} optionsSelect={[]}/>
+            <Filter
+              listSellers={[]}
+              sellersDashboard={[]}
+              startDatePicker={new Date()}
+              finalDate={new Date()}
+              locale={culture.locale}
+              optionsSelect={[]}
+            />
           </div>
         </PageBlock>
       </div>
       <div className="mt4">
         <PageBlock>
           <div className="mt4 mb7">
-            <Totalizer item={[{label: 'test', value: 0, iconBackgroundColor: '#EAFCE3', icon: <IconShoppingCart color="#79B03A" size={18} />}]} />
+            <Totalizer
+              item={[
+                {
+                  label: 'test',
+                  value: 0,
+                  iconBackgroundColor: '#EAFCE3',
+                  icon: <IconShoppingCart color="#79B03A" size={18} />,
+                },
+              ]}
+            />
           </div>
         </PageBlock>
       </div>
       <div className="mt4">
         <PageBlock>
           <div className="mt4 mb7">
-            {/*<TableComponent schemaTable={tempColumns} itemTable={[]} actions={[]}/>*/}
+            {/* <TableComponent schemaTable={tempColumns} itemTable={[]} actions={[]}/> */}
             <TableComponent />
           </div>
         </PageBlock>
--- react/components/Invoices/InvoiceTable.tsx/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const index = () => {
-  return <div />
-}
-
-export default index

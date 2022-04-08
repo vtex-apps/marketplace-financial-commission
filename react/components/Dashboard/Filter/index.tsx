9d645dd9f152f7dad4868723c30d1b1edcb9c871
Erika Jissel Gutierrez Beltran <erika.beltran@vtex.com.br>
2022-04-08 08:08:19 -0500

    Review table data

--- react/components/Dashboard/Filter/index.tsx
+++ react/components/Dashboard/Filter/index.tsx
@@ -1,70 +1,135 @@
 import type { FC } from 'react'
-import React from 'react'
-import type { InjectedIntlProps } from 'react-intl'
-import { injectIntl } from 'react-intl'
-import { formatIOMessage } from 'vtex.native-types'
-
+import React, { useState } from 'react'
+import { FormattedMessage } from 'react-intl'
 import {
   EXPERIMENTAL_Select as Select,
   DatePicker,
   ButtonWithIcon,
   IconFilter,
+  IconDelete,
+  ButtonGroup,
 } from 'vtex.styleguide'
-
-import { message } from '../../../utils/definedMessages'
-
+import { addDays } from 'date-fns'
 
 import styles from '../../../styles.css'
 
-const Filter: FC<FilterProps & InjectedIntlProps> = (props) => {
+const Filter: FC<FilterProps> = (props) => {
+  const [dataFilter, setDataFilter] = useState([])
+  const [startDateFilter, setDateFilter] = useState('')
+  const [finalDateFilter, setFinalDateFilter] = useState('')
 
-  return(
+  const getDate = (date: string) => {
+    const dateConverter = new Date(date)
+    const month = dateConverter.getMonth() + 1
+    const monthString = month <= 9 ? `0${month}` : month
+    const day = dateConverter.getDate() + 1
+    const dayString = day <= 9 ? `0${day}` : day
+
+    return `${dateConverter.getFullYear()}-${monthString}-${dayString}`
+  }
+
+  const changesValuesTable = () => {
+    const filterData = props.dataWithoutFilter?.filter((seller: any) =>
+      dataFilter?.some((item: any) => item.label === seller.name)
+    )
+
+    if (filterData?.length) {
+      props.setDataWithoutFilter(filterData)
+    } else {
+      props.setDataWithoutFilter([])
+    }
+
+    if (startDateFilter !== '') {
+      const newDateStart = getDate(startDateFilter)
+
+      console.info('newDateStart ', newDateStart)
+      props.setStartDate(newDateStart)
+    } else if (finalDateFilter !== '') {
+      const newDateFinal = getDate(finalDateFilter)
+
+      props.setFinalDate(newDateFinal)
+    }
+  }
+
+  const changeStartDate = (start: any) => {
+    setDateFilter(start)
+    /* if (props.finalDatePicker)
+      if (start.getTime() < props.finalDatePicker.getTime()) {
+        const newDate = getDate(start)
+
+        props.setStartDate(newDate)
+      } */
+  }
+
+  const changeFinalDate = (final: any) => {
+    if (startDateFilter !== '') setFinalDateFilter(final)
+    /* if (props.startDatePicker)
+      if (final.getTime() > props.startDatePicker.getTime()) {
+        const newDate = getDate(final)
+
+        props.setFinalDate(newDate)
+      } */
+  }
+
+  return (
     <div className={`${styles.filter} flex`}>
-      <div className={`${styles.filter_container} w-30 mr4`}>
-        <Select
-          multi
-          label={formatIOMessage({id: message.selectSeller.id, intl: props.intl,}).toString()}
-          options={props.listSellers}
-          onChange={() => {}}
-        />
+      <div className={`${styles.filter_container} w-50 mr4`}>
+        <div>
+          <Select
+            multi
+            label={<FormattedMessage id="admin/table.title-seller-label" />}
+            options={props.optionsSelect}
+            onChange={(values: any) => setDataFilter(values)}
+          />
+        </div>
       </div>
-      <div className={`${styles.filter_container} w-20 mr4`}>
-        <Select
-          multi
-          label={formatIOMessage({id: message.selectItems.id, intl: props.intl,}).toString()}
-          options={props.optionsSelect}
-          onChange={(values: any) => {console.info('values ', values)}}
-        />
-      </div>
-      <div className={`${styles.filter_container} w-20 mr4`}>
+      <div className={`${styles.filter_container} w-15`} id="datepicker-left">
         <DatePicker
-          label={formatIOMessage({id: message.startPicker.id, intl: props.intl,}).toString()}
-          value={props.startDatePicker}
-          onChange={() => { }}
+          label="Start"
+          value={
+            startDateFilter !== '' ? startDateFilter : props.startDatePicker
+          }
+          maxDate={addDays(new Date(), -1)}
+          onChange={(start: any) => changeStartDate(start)}
           locale={props.locale}
         />
       </div>
-      <div className={`${styles.filter_container} w-20 mr4`}>
+      <div
+        className={`${styles.filter_container} w-15 mr4`}
+        id="datepicker-right"
+      >
         <DatePicker
-          label={formatIOMessage({id: message.endPicker.id, intl: props.intl,}).toString()}
-          value={props.finalDate}
-          onChange={() => {}}
+          label="End"
+          value={
+            finalDateFilter !== '' ? finalDateFilter : props.finalDatePicker
+          }
+          maxDate={addDays(new Date(), -1)}
+          onChange={(final: any) => changeFinalDate(final)}
           locale={props.locale}
         />
       </div>
-      <div className={`${styles.btn_filter} w-10 mt6`}>
-        <ButtonWithIcon
-          icon={<IconFilter color="#FFF" size={18} />}
-          iconPosition="right"
-          variation="primary"
-          onClick={() => {}}
-        >
-          {formatIOMessage({id: message.filter.id, intl: props.intl,}).toString()}
-        </ButtonWithIcon>
+      <div className="w-20 mt6">
+        <ButtonGroup
+          buttons={[
+            // eslint-disable-next-line react/jsx-key
+            <ButtonWithIcon
+              isActiveOfGroup
+              onClick={() => changesValuesTable()}
+              icon={<IconFilter />}
+            >
+              {<FormattedMessage id="admin/table.title-filter" />}
+            </ButtonWithIcon>,
+            // eslint-disable-next-line react/jsx-key
+            <ButtonWithIcon
+              isActiveOfGroup={false}
+              onClick={() => {}}
+              icon={<IconDelete />}
+            />,
+          ]}
+        />
       </div>
     </div>
   )
 }
 
-export default injectIntl(Filter)
-
+export default Filter
--- react/components/Dashboard/Table/Tablev2/index.tsx
+++ react/components/Dashboard/Table/Tablev2/index.tsx
@@ -1,74 +1,15 @@
 import type { FC } from 'react'
 import React, { useMemo } from 'react'
-
 import {
   EXPERIMENTAL_Table as Table,
   EXPERIMENTAL_useTableMeasures as useTableMeasures,
-  EXPERIMENTAL_useTableSort as useTableSort
+  EXPERIMENTAL_useTableSort as useTableSort,
+  Spinner,
+  // EXPERIMENTAL_useCheckboxTree as useColumnsWithCheckboxes
 } from 'vtex.styleguide'
 
-
-const TableV2: FC = () => {
+const TableV2: FC<TableData> = (props) => {
   const sorting = useTableSort()
-  const columns = [
-    {
-      /** Prop that this column represents */
-      id: 'id',
-      /** Title that will appear on Header */
-      title: 'ID',
-      /** Fixed width */
-      width: '3rem',
-    },
-    {
-      id: 'name',
-      title: 'Name',
-      sortable: true,
-    },
-    {
-      id: 'qty',
-      title: 'Qty',
-    },
-    {
-      id: 'costPrice',
-      title: 'Cost',
-      /** Cellrenderer using the default data, which is costPrice in this case */
-    },
-    {
-      id: 'retailPrice',
-      title: 'Retail',
-    },
-    {
-      id: 'profit',
-      /** This is a custom title */
-      title: 'Profit',
-      /** Profit is a condensed column generated using retailPrice and costPrice props */
-    },
-    {
-      id: 'actions',
-      width: '3rem',
-      /** This column is extended, its data is the entire row */
-    },
-  ]
-
-  const arrayItems = [{
-    id: 'id',
-    name: 'test',
-    qty: 'qty',
-    costPrice: 'costPrice',
-    retailPrice: 'retailPrice',
-    profit: 'profit',
-    actions: 'actions'
-  },
-  {
-    id: 'id2',
-    name: 'name',
-    qty: 'qty',
-    costPrice: 'costPrice',
-    retailPrice: 'retailPrice',
-    profit: 'profit',
-    actions: 'actions'
-  }
-  ]
 
   /*eslint-disable */
   const ascOrdering = (prop:any): (a: any, b: any) => 1 | -1 | 0 => (a, b) => a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0
@@ -79,26 +20,44 @@ const TableV2: FC = () => {
       sorted: { order, by },
     } = sorting
     if (!order) {
-      return arrayItems
+      return props.items
     }
 
     const ascending = order === 'ASC'
     const comparator = ascending ? ascOrdering(by) : dscOrdering(by)
-    return arrayItems.sort(comparator)
+    return props.items.sort(comparator)
   }
-    , [sorting.sorted, arrayItems])
+    , [sorting.sorted, props.items])
 
   const measures = useTableMeasures({ size: items.length })
+
+  console.info('ITEMSSSSSSSSSSSSSS ', items)
+
+  const pagination = {
+    textOf: 'of',
+    rowsOptions: [5, 10, 15, 20],
+    textShowRows: 'Show rows',
+    totalItems: items.length,
+  }
 
   const ColumnsExample = () => {
     return (
       <Table
         measures={measures}
         items={items}
-        columns={columns}
+        columns={props.schemaTable}
         highlightOnHover
-        sorting={sorting}
-      />
+        sorting={sorting}>
+        <Table.Pagination {...pagination} />
+      </Table>
+    )
+  }
+
+  if (props.loading) {
+    return (
+      <div className="tc">
+        <Spinner />
+      </div>
     )
   }
 

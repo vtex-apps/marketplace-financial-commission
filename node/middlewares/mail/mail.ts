650b49db2dbde652cceaf81709b59b73e3ceff2e
afmejia23 <andres.moreno@vtex.com.br>
2022-04-22 11:04:50 -0500

    Adding handle-bars to invoice template

--- node/middlewares/mail/mail.ts
+++ node/middlewares/mail/mail.ts
@@ -6,14 +6,100 @@ export async function sendMail(ctx: Context, next: () => Promise<any>) {
   } = ctx
 
   const body = await json(ctx.req)
+  const { email } = body
+
+  if (!email) {
+    ctx.status = 400
+    ctx.body = 'Specify an email address'
+
+    return
+  }
+
+  // If users sends a json structure
+  let data = {}
+
+  if (body.jsonData) {
+    data = { ...body.jsonData }
+  } else {
+    const { invoiceId } = body
+
+    if (!invoiceId) {
+      ctx.status = 400
+      ctx.body = 'Specify an invoiceID'
+
+      return
+    }
+
+    console.info('Invoice id--------->')
+    console.info(invoiceId)
+
+    // TODO: Change mock invoice for real invoice
+    const invoice = {
+      id: 'sellerA_3918239129',
+      status: 'paid',
+      invoiceCreateDate: '25/02/2022',
+      invoiceDueDate: '15/03/2022',
+      sellerData: {
+        name: 'Seller A',
+        id: 'SellerId',
+        contact: {
+          phone: '+34874958678',
+          fax: null,
+          email: 'sesarocampo@sellera.com',
+        },
+        address: {
+          postalCode: 'EC1Y 8TZ',
+          city: 'Random',
+          state: 'State of Liberty',
+          country: 'GBR',
+          street: 'Carrer de Sardenya',
+          number: null,
+        },
+        comment: null,
+      },
+      orders: [
+        {
+          orderId: '10012931-12',
+          total: 1200.0,
+          commission: 400.0,
+          totalOrderRate: 0.3,
+        },
+        {
+          orderId: '11212924-14',
+          total: 500.0,
+          commission: 100.0,
+          totalOrderRate: 0.2,
+        },
+        {
+          orderId: '13312931-13',
+          total: 1500.0,
+          commission: 1000.0,
+          totalOrderRate: 0.6,
+        },
+      ],
+      totalizers: {
+        subtotal: 1500,
+        tax: {
+          type: 'percentage',
+          value: 10,
+        },
+        fee: 150,
+        total: 1800,
+      },
+    }
+
+    data = {
+      ...invoice,
+    }
+  }
 
   const sendEmailResponse = await mail.sendMail({
     templateName: 'invoice-detail',
     jsonData: {
       message: {
-        to: body.email,
+        to: email,
       },
-      ...body.jsonData,
+      ...data,
     },
   })
 

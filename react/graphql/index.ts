89142e26026f8f6a46dbc0b42503f2d53cfd40dc
Raphael Saez <raphaelsaezz@gmail.com>
2022-04-26 17:32:07 -0500

    wip: settings detail

--- react/graphql/index.ts
+++ react/graphql/index.ts
@@ -2,5 +2,6 @@ import SEARCH_ORDERS from './getSearchOrders.gql'
 import SEARCH_SELLERS from './getSearchSellers.gql'
 import SEARCH_STATS from './getSearchStats.gql'
 import GET_SELLERS from './getSellers.gql'
+import GET_TOKEN from './getToken.gql'
 
-export { SEARCH_ORDERS, SEARCH_SELLERS, SEARCH_STATS, GET_SELLERS }
+export { SEARCH_ORDERS, SEARCH_SELLERS, SEARCH_STATS, GET_SELLERS, GET_TOKEN }

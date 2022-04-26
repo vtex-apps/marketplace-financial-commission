2ec814be1f6ab547c2a9db448fc8279a351cfc3d
deimer.marin <deimer.marin@vtex.com.br>
2022-04-26 17:55:44 -0500

    Commit try catch for error to Generate

--- node/middlewares/dashboard/generate/generate.ts
+++ node/middlewares/dashboard/generate/generate.ts
@@ -15,6 +15,7 @@ export async function generate(ctx: Context, next: () => Promise<Dashboards>) {
       body: { sellers },
     },
     clients: { sellersDashboardClientMD, statisticsDashboardClientMD },
+    vtex: { logger },
   } = ctx
 
   const start = ctx.query.dateStart as string
@@ -27,112 +28,124 @@ export async function generate(ctx: Context, next: () => Promise<Dashboards>) {
   console.info(`Number Days-------> ${numDays}`)
 
   const processGenerate = async () => {
-    const getDates = getDatesInvoiced({
-      start,
-      end,
-    })
+    try {
+      const getDates = getDatesInvoiced({
+        start,
+        end,
+      })
 
-    let loop = new Date(getDates.dateInvoiceInitial)
-    const endLoop = new Date(getDates.dateInvoiceEnd)
-    const responseSellersMD = []
-    const responseStatisticsMD = []
+      let loop = new Date(getDates.dateInvoiceInitial)
+      const endLoop = new Date(getDates.dateInvoiceEnd)
+      const responseSellersMD = []
+      const responseStatisticsMD = []
 
-    // eslint-disable-next-line no-console
-    console.time('generateLoop')
-    while (loop <= endLoop) {
-      const [dayToProcess] = loop.toISOString().split('T')
-      const dateRange: DateRange = {
-        start: dayToProcess,
-        end: dayToProcess,
-      }
+      // eslint-disable-next-line no-console
+      console.time('generateLoop')
+      while (loop <= endLoop) {
+        const [dayToProcess] = loop.toISOString().split('T')
+        const dateRange: DateRange = {
+          start: dayToProcess,
+          end: dayToProcess,
+        }
 
-      const responseCalculateSellers = await calculateSellers(
-        ctx,
-        sellers,
-        dateRange
-      )
-
-      const {
-        sellersDashboard,
-        statistics: {
-          ordersCount,
-          totalComission,
-          totalOrderValue,
-          totalDiscounts,
-          totalOrdersItems,
-          totalShipping,
-          totalTax,
-        },
-      } = responseCalculateSellers
-
-      const dashboard: SellersDashboard = {
-        dateCut: dayToProcess,
-        sellers: sellersDashboard as [],
-        idGenerate: createKeyToken(),
-      }
-
-      const dashboardWithId = {
-        id: `DSH-${ctx.vtex.account}-${dayToProcess}`,
-        ...dashboard,
-      }
-
-      const dashboardSaveMD = await sellersDashboardClientMD.saveOrUpdate(
-        dashboardWithId
-      )
-
-      responseSellersMD.push(dashboardSaveMD)
-
-      const statsGeneral: StatisticsDashboard = {
-        dateCut: dayToProcess,
-        statistics: {
-          ordersCount,
-          totalComission,
-          totalOrderValue,
-          totalDiscounts,
-          totalOrdersItems,
-          totalShipping,
-          totalTax,
-        },
-        idStatistics: createKeyToken(),
-      }
-
-      const dashboardstatsWithId = {
-        id: `DSH-Statistics-${ctx.vtex.account}-${dayToProcess}`,
-        ...statsGeneral,
-      }
-
-      let responseStats
-
-      try {
-        responseStats = await statisticsDashboardClientMD.saveOrUpdate(
-          dashboardstatsWithId
+        const responseCalculateSellers = await calculateSellers(
+          ctx,
+          sellers,
+          dateRange
         )
-        responseStatisticsMD.push(responseStats)
-      } catch (err) {
-        const error = err as any
-        const { message, status, payload } = error
 
-        responseStats = { message, status, payload }
+        const {
+          sellersDashboard,
+          statistics: {
+            ordersCount,
+            totalComission,
+            totalOrderValue,
+            totalDiscounts,
+            totalOrdersItems,
+            totalShipping,
+            totalTax,
+          },
+        } = responseCalculateSellers
 
-        responseStatisticsMD.push(responseStats)
+        const dashboard: SellersDashboard = {
+          dateCut: dayToProcess,
+          sellers: sellersDashboard as [],
+          idGenerate: createKeyToken(),
+        }
+
+        const dashboardWithId = {
+          id: `DSH-${ctx.vtex.account}-${dayToProcess}`,
+          ...dashboard,
+        }
+
+        const dashboardSaveMD = await sellersDashboardClientMD.saveOrUpdate(
+          dashboardWithId
+        )
+
+        responseSellersMD.push(dashboardSaveMD)
+
+        const statsGeneral: StatisticsDashboard = {
+          dateCut: dayToProcess,
+          statistics: {
+            ordersCount,
+            totalComission,
+            totalOrderValue,
+            totalDiscounts,
+            totalOrdersItems,
+            totalShipping,
+            totalTax,
+          },
+          idStatistics: createKeyToken(),
+        }
+
+        const dashboardstatsWithId = {
+          id: `DSH-Statistics-${ctx.vtex.account}-${dayToProcess}`,
+          ...statsGeneral,
+        }
+
+        let responseStats
+
+        try {
+          responseStats = await statisticsDashboardClientMD.saveOrUpdate(
+            dashboardstatsWithId
+          )
+          responseStatisticsMD.push(responseStats)
+        } catch (err) {
+          const error = err as any
+          const { message, status, payload } = error
+
+          responseStats = { message, status, payload }
+
+          responseStatisticsMD.push(responseStats)
+        }
+
+        const newDate = loop.setDate(loop.getDate() + 1)
+
+        loop = new Date(newDate)
       }
 
-      const newDate = loop.setDate(loop.getDate() + 1)
+      const responseGenerateDashboard = {
+        Sellers: responseSellersMD,
+        Statistics: responseStatisticsMD,
+      }
 
-      loop = new Date(newDate)
+      // eslint-disable-next-line no-console
+      console.timeEnd('generateLoop')
+      console.info('Process completed')
+
+      logger.info({
+        message: 'Process completed',
+        data: `dateStart: ${start} - dateEnd: ${end}`,
+      })
+
+      return responseGenerateDashboard
+    } catch (error) {
+      logger.error({
+        message: `Error while completing the process ----> ${error} `,
+        data: `dateStart: ${start} - dateEnd: ${end}`,
+      })
+      throw error
     }
-
-    const responseGenerateDashboard = {
-      Sellers: responseSellersMD,
-      Statistics: responseStatisticsMD,
-    }
-
-    // eslint-disable-next-line no-console
-    console.timeEnd('generateLoop')
-
-    console.info(`Process completed`)
-
-    return responseGenerateDashboard
   }
 
   if (numDays > 5) {

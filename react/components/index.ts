36dbe6479637bfe92a32deb761717238cd850a7c
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-05-26 12:26:33 +0200

    feat: removed unused components

--- react/components/index.ts
+++ react/components/index.ts
@@ -1,6 +1,5 @@
-import EmptyTable from './Dashboard/EmptyTable'
 import Totalizer from './Dashboard/Totalizer'
 import Filter from './Dashboard/Filter'
 import SettingsDashboard from './Dashboard/SettingsDashboard'
 
-export { Totalizer, Filter, SettingsDashboard, EmptyTable }
+export { Totalizer, Filter, SettingsDashboard }

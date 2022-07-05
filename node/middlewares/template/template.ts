a58245c78a166d54a78c1330fb4bda6dae7add72
deimer.marin <deimer.marin@vtex.com.br>
2022-07-05 15:55:28 -0500

    Commit create endpoint for integration external

--- node/middlewares/template/template.ts
+++ node/middlewares/template/template.ts
@@ -1,21 +1,49 @@
-import templateBody from '../../templates'
+import GetBody from '../../templates'
+import { typeIntegration } from '../../utils/typeIntegration'
 
 export async function templateMethod(ctx: Context, next: () => Promise<any>) {
   const {
     clients: { template },
   } = ctx
 
-  const templateResponse = await template.getTemplate()
+  let templateResponse = ''
 
-  ctx.set('Cache-Control', 'no-cache')
+  const selectTemplate = async (typeIntegrationValue: TypeIntegration) => {
+    switch (typeIntegrationValue) {
+      case TypeIntegration.external: {
+        templateResponse = await template.getTemplateExternal()
+
+        return templateResponse
+      }
+
+      case TypeIntegration.internal: {
+        templateResponse = await template.getTemplate()
+
+        return templateResponse
+      }
+
+      default: {
+        templateResponse = await template.getTemplate()
+
+        return templateResponse
+      }
+    }
+  }
+
+  const integration = await typeIntegration(ctx)
+
+  await selectTemplate(integration)
+
   if (templateResponse) {
     ctx.body = { template: templateResponse }
   } else {
     // Create template
+    const templateBody = await GetBody(ctx)
     const templateCreated = await template.publishTemplate(templateBody)
 
     ctx.body = { template: templateCreated }
   }
 
+  ctx.set('Cache-Control', 'no-cache')
   await next()
 }

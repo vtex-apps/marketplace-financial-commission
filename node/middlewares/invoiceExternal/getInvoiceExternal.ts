bd9addc9726d974562cbaa7736cb5ffbecedda3a
deimer.marin <deimer.marin@vtex.com.br>
2022-07-06 18:26:07 -0500

    Commit List Invoices

--- node/middlewares/invoiceExternal/getInvoiceExternal.ts
+++ node/middlewares/invoiceExternal/getInvoiceExternal.ts
@@ -1,4 +1,42 @@
-import { config } from '../../constants'
+const assembleWhere = async (query: any): Promise<string> => {
+  const sellerName = (query.sellerName ?? '') as string
+  const sellerId = (query.sellerId ?? '') as string
+  const status = (query.status ?? '') as string
+  const createdDateInitial = (query.createdDateInitial ?? '') as string
+  const createdDateEnd = (query.createdDateEnd ?? '') as string
+
+  let where = ''
+
+  if (sellerName !== '') {
+    where += `seller.name="${sellerName}"`
+  }
+
+  if (sellerId !== '') {
+    if (sellerName !== '') {
+      where += ` AND seller.id="${sellerId}"`
+    } else {
+      where += `seller.id="${sellerId}"`
+    }
+  }
+
+  if (status !== '') {
+    if (sellerName !== '' || sellerId !== '') {
+      where += ` AND status="${status}"`
+    } else {
+      where += `status="${status}"`
+    }
+  }
+
+  if (createdDateInitial !== '' && createdDateEnd !== '') {
+    if (sellerName !== '' || sellerId !== '' || status !== '') {
+      where += ` AND (createdDate between ${createdDateInitial} AND ${createdDateEnd})`
+    } else {
+      where += `(createdDate between ${createdDateInitial} AND ${createdDateEnd})`
+    }
+  }
+
+  return where
+}
 
 export async function getInvoiceExternal(
   ctx: Context,
@@ -10,28 +48,62 @@ export async function getInvoiceExternal(
         params: { id },
       },
     },
-    clients: { vbase },
+    query,
+    clients: { externalInvoices },
   } = ctx
 
-  let resultList
-
-  console.info('🚀 ~ file: getInvoiceExternal.ts ~ line 19 ~ id', id)
+  let sellerInvoices
 
   if (id !== '') {
-    console.info('Entre aca getFile')
+    const pagination = {
+      page: 1,
+      pageSize: 100,
+    }
 
-    resultList = await vbase.getFile(
-      config.INVOICE_EXTERNAL_BUCKET,
-      id as string
+    sellerInvoices = await externalInvoices.searchRaw(
+      pagination,
+      ['id,status,accountName,seller,createdDate,jsonData'],
+      'createdIn',
+      `id=${id}`
     )
   } else {
-    console.info('Entre aca listFiles')
+    const page = Number(
+      (ctx.query.page === '' || ctx.query.page === undefined
+        ? '1'
+        : ctx.query.page) as string
+    )
 
-    resultList = (await vbase.listFiles(config.INVOICE_EXTERNAL_BUCKET)).data
+    const pageSize = Number(
+      (ctx.query.pageSize === '' || ctx.query.pageSize === undefined
+        ? '100'
+        : ctx.query.pageSize) as string
+    )
+
+    const where = await assembleWhere(query)
+
+    const pagination = {
+      page,
+      pageSize,
+    }
+
+    if (where !== '') {
+      sellerInvoices = await externalInvoices.searchRaw(
+        pagination,
+        ['id,status,accountName,seller,createdDate,jsonData'],
+        'createdIn',
+        where
+      )
+    } else {
+      sellerInvoices = await externalInvoices.searchRaw(
+        pagination,
+        ['id,status,accountName,seller,createdDate,jsonData'],
+        'createdIn'
+      )
+    }
   }
 
   ctx.status = 200
-  ctx.body = { resultList }
+  ctx.body = sellerInvoices
   ctx.set('Cache-Control', 'no-cache ')
 
   await next()

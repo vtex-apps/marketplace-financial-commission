7bb24e63f89d6218bc6c5ebad1a576115117e3f0
deimer.marin <deimer.marin@vtex.com.br>
2022-07-07 18:29:55 -0500

    Commit endpoint to be used by the seller for external integration

--- node/middlewares/policy.ts
+++ node/middlewares/policy.ts
@@ -1,13 +1,17 @@
 export async function policy(ctx: Context, next: () => Promise<any>) {
   const {
     state: {
-      body: { seller },
+      body: {
+        seller,
+        params: { id },
+      },
     },
   } = ctx
 
   const { name } = seller as Seller
 
   ctx.query.sellerName = name
+  ctx.vtex.route.params.id = id as string
 
   await next()
 }

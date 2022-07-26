62e0a057dd3e854fba9eedc9fd322b0e98327190
Erika Jissel Gutierrez Beltran <erika.beltran@vtex.com.br>
2022-07-26 17:38:29 -0500

    remove comments

--- react/commissionReport.tsx
+++ react/commissionReport.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { report as Report } from 'vtex.components-financial-commission'
+import { commissionReport as Report } from 'vtex.components-financial-commission'
 
 import { GET_SELLERS, SEARCH_STATS, SEARCH_SELLERS } from './graphql'
 
